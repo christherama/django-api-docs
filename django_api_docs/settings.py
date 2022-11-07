170fffed9b8ccf24366a9343e0f58b76e9892dd4
Chris Ramacciotti <chris@rama.io>
2022-11-06 21:02:29 -0600

    Restores settings

--- django_api_docs/settings.py
+++ django_api_docs/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
-import pathlib
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.getenv("STATIC_ROOT", f"{BASE_DIR}/static/")
-STATIC_URL = "/static/"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
